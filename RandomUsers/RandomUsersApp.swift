d7ea6a932d360390ae3466d41951dc8f3e50dccb
Yasin Şükrü TAN <yasinsukrutan@gmail.com>
2022-12-09 12:19:51 +0300

    Initial Commit

---
+++ RandomUsers/RandomUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUsersApp.swift
+//  RandomUsers
+//
+//  Created by Yasin Şükrü Tan on 9.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct RandomUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
